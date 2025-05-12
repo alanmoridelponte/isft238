012a9a6d1bef60ad322d2f02741980a556c45236
Alan Mori Delponte <alan@odiseasoft.com>
2025-05-11 22:53:17 -0300

    feat(feature/frontend): update banner item css classes

--- resources/views/components/public/banner.blade.php
+++ resources/views/components/public/banner.blade.php
@@ -2,12 +2,12 @@
     <div class="container mx-auto px-4 py-2 flex justify-between items-center">
         <div class="flex space-x-4">
             @foreach ($leftItems as $item)
-            <a href="{{ $item['href'] }}" class="text-sm flex items-center hover:text-blue-200">
+            <a href="{{ $item['href'] }}" class="text-xs md:text-sm flex items-center hover:text-blue-200">
                 <i class="fas {{ $item['icon'] }} mr-2"></i>{{ $item['name'] }}
             </a>
             @endforeach
         </div>
-        <div class="flex space-x-4">
+        <div class="flex space-x-4 hidden md:block">
             @foreach ($rightItems as $item)
             <a href="{{ $item['href'] }}" class="text-sm hover:text-blue-200 tracking-tighter">
                 {{ $item['name'] }}<i class="fas {{ $item['icon'] }} ml-1"></i>
