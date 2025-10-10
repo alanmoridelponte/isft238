bcfa96e1af3447c75c718c7b10ae818c16de5345
Alan Mori Delponte <alan@odiseasoft.com>
2025-10-10 16:19:09 -0300

    feat: update career list card auto-grow height

--- resources/views/components/public/careers-list-card.blade.php
+++ resources/views/components/public/careers-list-card.blade.php
@@ -1,8 +1,8 @@
-<div class="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300">
+<div class="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300 flex flex-col h-full">
     <div class="bg-green-800 h-3"></div>
-    <div class="p-4 md:pb-3 md:px-6">
+    <div class="p-4 md:pb-3 md:px-6 flex flex-col flex-grow">
         <h3 class="text-xl font-bold text-green-800 mb-2">{{ $career->title }}</h3>
-        <p class="text-gray-600 mb-4">{{ $career->excerpt }}</p>
+        <p class="text-gray-600 mb-4 flex-grow">{{ $career->excerpt }}</p>
         <div class="mb-4 flex flex-wrap justify-start">
             <span class="inline-block bg-amber-100 text-amber-800 text-xs font-semibold px-2 py-1 rounded mr-2 mb-2">
                 <i class="far fa-clock mr-0"></i>
@@ -13,8 +13,10 @@
                 Modalidad {{ $career->modality }}
             </span>
         </div>
-        <div class="flex items-center text-sm text-gray-700 justify-start">
-            <a href="{{ route('careers.show', ['career' => $career->slug ?? 'not-found']) }}" class="inline-block bg-green-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg transition duration-300">Más información</a>
+        <div class="mt-auto flex items-center text-sm text-gray-700 justify-start">
+            <a href="{{ route('careers.show', ['career' => $career->slug ?? 'not-found']) }}" class="inline-block bg-green-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg transition duration-300">
+                Más información
+            </a>
         </div>
-     </div>
+    </div>
 </div>
